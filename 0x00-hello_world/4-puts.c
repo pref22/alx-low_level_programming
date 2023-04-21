623cd5c87c3756e0c084a97ef8d7e2c64a9fa3c5
Prefna Lebongo <lebongop@gmail.com>
2023-04-21 12:07:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - a C progaram that prints a variable wit the variable puts
+ * Return: 0 (Return 0)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
