b8bd0bdad3e049cbe78f29db5b47ff0b55fac880
Prefna Lebongo <lebongop@gmail.com>
2023-04-21 14:04:07 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 		n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n". n);
 	}
 	else if (n == 0)
 	{
@@ -20,7 +21,7 @@ int main(void)
 	}
 	else 
 	{
-	printf("%d is negative\n", n);
+	printf("%d is negative\n". n);
 	}
 	return (0);
 }
