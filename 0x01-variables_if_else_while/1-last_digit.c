d5b6def1722f3b2eea491fe2db13b8196c712b76
Prefna Lebongo <lebongop@gmail.com>
2023-04-24 12:34:37 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - A program that prints last digit of the number stored
+ * main - Entry point
  *
  * Return: Always 0 (SUCCESS)
  */
