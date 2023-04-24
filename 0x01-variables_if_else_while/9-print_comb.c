e7593b827388e7c42a732884e8ba184854b6313b
Prefna Lebongo <lebongop@gmail.com>
2023-04-24 14:29:29 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-int c = 0;
+int n;
 
-while (c < 10)
+for (n = 48; n < 58; n++)
 {
-putchar(48 + c);
-if (c != 9)
+putchar(n;)
+if (n != 57)
 {
 putchar(',');
 putchar(' ');
 }
-c++;
+putchar('\n');
+return (0);
 }
