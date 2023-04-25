b4d5c5710af9e390fef8684d60e39da754fec236
Prefna Lebongo <lebongop@gmail.com>
2023-04-25 15:37:52 -0700

    a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that multiples of 3 0r 5.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int p, sum = 0;
+
+	for (p = 0; p < 1024; p++)
+       	{
+	       	if ((p % 3) == 0 || (p % 5) == 0)
+			sum += p;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
