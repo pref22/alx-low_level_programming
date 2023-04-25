0d93e01a8bce213c20b41ac555ae8b07430bdac5
Prefna Lebongo <lebongop@gmail.com>
2023-04-25 06:35:50 -0700

    a function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islover - check if char is lowercase
+ * Return: 1 if char is lowercase, otherwise 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else 
+		return (0);
+}
