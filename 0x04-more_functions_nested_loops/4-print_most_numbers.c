51262c237398da2e1174c43858a9bff488a30e44
Prefna Lebongo <lebongop@gmail.com>
2023-04-27 08:30:08 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers, from 0 to 9,
+ * followed by a new line.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar((n % 10) + '0');
+	}
+
+	_putchar('\n');
+}
