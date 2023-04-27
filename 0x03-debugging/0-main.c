b34df37bb4d6323d81083cdd1f343c3496cd3a6c
Prefna Lebongo <lebongop@gmail.com>
2023-04-27 15:23:28 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Tests function that prints if integer is positive or negative
+ * main - Tests function that prints if integer is positive_or_negative
  * Return: 0
  */
 
