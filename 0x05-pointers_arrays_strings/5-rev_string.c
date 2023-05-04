8fca5c55e453526f77a6a9cdd838fe40d07c8fbc
Prefna Lebongo <lebongop@gmail.com>
2023-05-03 18:10:59 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
  * rev_string - Reverses a string
- * @s: The string to ne modified
+ * @s: The string to be modified
  * Return: void
  */
 void rev_string(char *s)
@@ -10,14 +10,13 @@ void rev_string(char *s)
 	int len = 0, index = 0;
 	char tmp;
 
-	while (s[index++]))
-		len++;
+	while (s[index++])
+	len++;
 
-	for (index = len - 1; index >= len / 2; index__)
+	for (index = len - 1; index >= len / 2; index--)
 	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
+	tmp = s[index];
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
 	}
 }
-
