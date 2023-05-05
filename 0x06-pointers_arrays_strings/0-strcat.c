b6e608b3f134a65f91506b0fe4281ca047b6bfba
Prefna Lebongo <lebongop@gmail.com>
2023-05-05 02:40:05 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * memset - Fills the first n bytes of the memory aea
- * pointed to by s with the constant byte c.
+ * memset - Fills the first n bytes of the memory area
+ * pointed to by @s with the constant byte @c.
  * @s: A point to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
- * return: A pointer to the filled memory area @s.
+ * Return: A pointer to the filled memory area @s with.
  */
 void *_memset(void *s, int c, size_t n)
 {
