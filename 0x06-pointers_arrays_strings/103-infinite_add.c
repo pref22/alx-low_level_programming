6e0b2c5c6072a62f6adff04ecb0e85fc0114226e
Prefna Lebongo <lebongop@gmail.com>
2023-05-09 14:34:30 -0700

    commit the changes

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,22 +11,22 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int count, count2;
+int count, count2;
 
-	while (n1[count] != '\0')
-		count++;
-	while (n2[count2] != '\0')
-		count2++;
+while (n1[count] != '\0')
+count++;
+while (n2[count2] != '\0')
+count2++;
 
-	*r = *(r + size_r);
-	while (n1[count] > 0 || n1[count2] > 0)
-	{
-		if (n1[count] + n2[count2] > 0)
-			*r = n1[count - 1] + n2[count2 - 1] + 1;
-		else
-			*r = n1[count] + n2[count2];
-		count--;
-		count2--;
-	}
-	return (r);
+*r = *(r + size_r);
+while (n1[count] > 0 || n1[count2] > 0)
+{
+if (n1[count] + n2[count2] > 0)
+*r = n1[count - 1] + n2[count2 - 1] + 1;
+else
+*r = n1[count] + n2[count2];
+count--;
+count2--;
+}
+return (r);
 }
